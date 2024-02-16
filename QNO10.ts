56d8cf0c65bb415f789858d320c8ec4658a8ec9a
Aqsagull99 <159878357+Aqsagull99@users.noreply.github.com>
2024-02-17 00:34:25 +0500

    Add files via upload

---
+++ QNO10.ts
@@ -0,0 +1,28 @@
+// //  QNO:10
+// Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+let myname:string="Aqsa gull"
+let currentdate:number= 16_2_2004;
+
+ let pakistanteam =["babar azam", " fakhar zaman" , " shahdab khan" ," muhammad rsizwan","Mamir","iftekhar chacha"];
+
+pakistanteam.push("rehman malik" , "aqsa")
+
+console.log(pakistanteam)
+
+
+let num1:number =12;
+let num2:number =4;
+console.log(num1 - num2)
+
+
+console.log(`${myname},${currentdate},  \n we will use push method for adding new value in ending  \n and use subtraction for less from in any values `)
+
+
+
+
+
+
+
+
+
---
+++ qno10.js
@@ -0,0 +1,11 @@
+// //  QNO:10
+// Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+var myname = "Aqsa gull";
+var currentdate = 1622004;
+var pakistanteam = ["babar azam", " fakhar zaman", " shahdab khan", " muhammad rsizwan", "Mamir", "iftekhar chacha"];
+pakistanteam.push("rehman malik", "aqsa");
+console.log(pakistanteam);
+var num1 = 12;
+var num2 = 4;
+console.log(num1 - num2);
+console.log("".concat(myname, ",").concat(currentdate, ",  \n we will use push method for adding new value in ending  \n and use subtraction for less from in any values "));
